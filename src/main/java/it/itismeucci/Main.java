588c67753023c1f500a7445b46b9fc65044a454b
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-10-09 08:53:28 +0200

    Client created

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,13 @@
 package it.itismeucci;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Il client è partito!");
+        Socket s = new Socket("127.0.0.1", 3000);
+        System.out.println("Il client si è collegato!");
     }
 }
