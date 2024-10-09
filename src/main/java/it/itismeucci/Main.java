8c3ff07310bfbd06b6e2f9e563d60851348581ea
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-10-09 09:14:48 +0200

    Comunicazione col server

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,6 +1,9 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -9,5 +12,15 @@ public class Main {
         System.out.println("Il client è partito!");
         Socket s = new Socket("127.0.0.1", 3000);
         System.out.println("Il client si è collegato!");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        out.writeBytes("Ciao sono il client!\n");
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("Stringa ricevuta: " + stringaRicevuta);
+
+        s.close();
     }
 }
